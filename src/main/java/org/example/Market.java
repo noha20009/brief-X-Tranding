7a8d6aac180124423cc193958485a732e519bad7
noha20009 <machkourinouhayla@gmail.com>
2026-01-27 17:44:45 +0100

    l'ajoute de foncion ajouter trader

--- src/main/java/org/example/Market.java
+++ src/main/java/org/example/Market.java
@@ -4,13 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Market {
-    private List<Trader> listTrider;
+    private List<Trader> listTrader;
     private List<Asset> listAsset;
     private List<Transaction> listTransaction;
 
     public Market(List<Trader> listTrider, List<Asset> listAsset, List<Transaction> listTransaction) {
-        this.listTrider = new ArrayList<>();
+        this.listTrader = new ArrayList<>();
         this.listAsset = new ArrayList<>();
         this.listTransaction = new ArrayList<>();
     }
+
+    public void ajouterTrader(Trader trader){
+
+            if (listTrader.contains(trader)){
+                System.out.println("ce trader n'existe pas");
+            }else {
+              listTrader.add(trader);
+                System.out.println("trader ajouté avec succés");
+            }
+    }
+
+    public List<Trader> getListTrader() {
+        return listTrader;
+    }
+
+    public ajouterAssets(){
+
+    }
+
+
 }
