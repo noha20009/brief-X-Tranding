e840e5d438894390439e3c0106cc7b1a4d1fc1b0
noha20009 <machkourinouhayla@gmail.com>
2026-01-27 15:13:22 +0100

    l'ajoute de constracteur et les attributs

--- src/main/java/org/example/CryptoCurrency.java
+++ src/main/java/org/example/CryptoCurrency.java
@@ -1,4 +1,7 @@
 package org.example;
 
-public class CryptoCurrency {
+public class CryptoCurrency extends Asset {
+    public CryptoCurrency(int idAsset, String nom, double prixUnitaire, String type) {
+        super(idAsset, nom, prixUnitaire, type);
+    }
 }
--- src/main/java/org/example/Stock.java
+++ src/main/java/org/example/Stock.java
@@ -1,4 +1,7 @@
 package org.example;
 
-public class Stock {
+public class Stock extends Asset {
+    public Stock(int idAsset, String nom, double prixUnitaire, String type) {
+        super(idAsset, nom, prixUnitaire, type);
+    }
 }
